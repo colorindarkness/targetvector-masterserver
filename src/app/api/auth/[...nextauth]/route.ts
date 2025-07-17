e45793db3fc718f5811895b3a90da8b857da5f11
colorindarkness <urielballinas@gmail.com>
2025-07-17 02:06:26 -0700

    Begin implementing Conditional rendering cont.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { User } from 'next-auth'
 import { authOptions } from '@/app/api/auth/option'
+import { revalidatePath } from 'next/cache'
 
 declare module 'next-auth' {
   interface User {
@@ -24,6 +25,7 @@ declare module 'next-auth/jwt' {
   }
 }
 
+revalidatePath('User')
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
