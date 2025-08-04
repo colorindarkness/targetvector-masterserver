4244b4d03b047099b86781affcd2a2ea47fb073b
colorindarkness <urielballinas@gmail.com>
2025-08-04 16:30:32 -0700

    Modify Login

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ searchParams }: { searchParams: SearchParam
     <Row className="justify-content-center align-items-center px-3">
       <Col lg={20}>
         <Row>
-          <Col md={13} className="bg-white dark:bg-dark border p-5">
+          <Col md={10} className="bg-white dark:bg-dark border p-5">
             <div>
               <h1>{dict.login.title}</h1>
               <Row>
@@ -60,7 +60,7 @@ export default async function Page({ searchParams }: { searchParams: SearchParam
             </div>
           </Col>
           <Col
-            md={7}
+            md={5}
             className="bg-primary text-white d-flex align-items-center justify-content-center p-5"
           >
             <div className="text-center">
